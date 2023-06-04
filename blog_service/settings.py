a62b6edf113f4af473920cf84aad963b0046626b
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-06-04 19:52:27 +0300

    Added Author model as AbstractUser

--- blog_service/settings.py
+++ blog_service/settings.py
@@ -108,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.Author"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from user.models import Author
+
+admin.site.register(Author, UserAdmin)
