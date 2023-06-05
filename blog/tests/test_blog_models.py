3e7106f02f692a397e8d9a766f3107fff590971f
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-06-05 19:23:21 +0300

    Added tests to user & blog apps

---
+++ blog/tests/test_blog_models.py
@@ -0,0 +1,72 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from blog.models import Category, Post, Comment
+
+
+class CategoryModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Category.objects.create(name="Programming")
+
+    def test_category_str(self):
+        category = Category.objects.get(id=1)
+
+        self.assertEqual(str(category), f"{category.name}")
+
+    def test_get_absolute_url(self):
+        category = Category.objects.get(id=1)
+
+        self.assertEqual(category.get_absolute_url(), "/blog/")
+
+
+class PostModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        author = get_user_model().objects.create_user(
+            username="TestUser",
+            password="user12345"
+        )
+        category = Category.objects.create(name="Programming")
+
+        Post.objects.create(
+            author=author, category=category, title="About Django"
+        )
+
+    def test_post_str(self):
+        post = Post.objects.get(id=1)
+
+        self.assertEqual(str(post), f"{post.title}")
+
+    def test_get_absolute_url(self):
+        post = Post.objects.get(id=1)
+
+        self.assertEqual(post.get_absolute_url(), "/blog/")
+
+
+class CommentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        author = get_user_model().objects.create_user(
+            username="TestUser",
+            password="user12345"
+        )
+        category = Category.objects.create(name="Programming")
+
+        post = Post.objects.create(
+            author=author, category=category, title="About Django"
+        )
+        Comment.objects.create(
+            post=post, author_name="User User", content="Comment Test"
+        )
+
+    def test_category_str(self):
+        comment = Comment.objects.get(id=1)
+
+        self.assertEqual(
+            str(comment),
+            f"{comment.post.title} - {comment.author_name} {comment.date_added}"
+        )
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from user.models import Profile
+
+
+class ProfileModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = get_user_model().objects.create_user(
+            username="TestUser",
+            password="user12345"
+        )
+        Profile.objects.create(user=user)
+
+    def test_profile_str(self):
+        profile = Profile.objects.get(id=1)
+
+        self.assertEqual(str(profile), f"{profile.user}")
+
+    def test_get_absolute_url(self):
+        profile = Profile.objects.get(id=1)
+
+        self.assertEqual(profile.get_absolute_url(), "/blog/")
